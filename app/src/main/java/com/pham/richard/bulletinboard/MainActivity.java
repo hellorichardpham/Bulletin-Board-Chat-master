6032af42a896d4e54652d9a7601288b9d623182a
tiluong <tiluong@ucsc.edu>
2016-01-30 15:18:53 -0800

    m

--- app/src/main/java/com/pham/richard/bulletinboard/MainActivity.java
+++ app/src/main/java/com/pham/richard/bulletinboard/MainActivity.java
@@ -2,6 +2,7 @@ package com.pham.richard.bulletinboard;
 
 import android.widget.Button;
 import android.widget.EditText;
+import android.os.CountDownTimer;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -27,12 +28,18 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.concurrent.TimeUnit;
+import android.telephony.SmsManager;
+import android.location.LocationManager;
+import android.location.LocationListener;
 
 import java.util.UUID;
 
 
 import com.pham.richard.bulletinboard.R;
 import com.google.gson.Gson;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -44,7 +51,15 @@ public class MainActivity extends ActionBarActivity {
     Button mButton;
     EditText mEdit;
     EditText sEdit;
+    TextView cEdit;
     int totalTime = 0;
+    private static final String FORMAT = "%02d:%02d:%02d";
+    SmsManager smsManager = SmsManager.getDefault();
+    EditText txtphoneNo;
+    EditText txtMessage;
+    boolean countdownHasStarted = false;
+    double longitude;
+    double latitude;
 
     /*** Called when the activity is first created.*/
     @Override
@@ -55,8 +70,41 @@ public class MainActivity extends ActionBarActivity {
         mButton = (Button) findViewById(R.id.button);
         mEdit = (EditText) findViewById(R.id.minuteView);
         sEdit = (EditText) findViewById(R.id.secondView);
+        txtphoneNo = (EditText) findViewById(R.id.phoneNumber);
+        txtMessage = (EditText) findViewById(R.id.message);
+
+
+        final LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+
+        };
+
+
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        final double latitude = location.getLatitude();
+        final double longitude = location.getLongitude();
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
+
+        System.out.println("Latitude: " + latitude + "longitude: " + longitude);
 
         mButton.setOnClickListener(
+
                 new View.OnClickListener() {
                     public void onClick(View view) {
                         Log.v("TIME??", mEdit.getText().toString());
@@ -66,8 +114,78 @@ public class MainActivity extends ActionBarActivity {
                         totalTime += Integer.parseInt(sEdit.getText().toString()) * 1000;
                         System.out.println("totalTime: " + totalTime);
 
+                        cEdit = (TextView) findViewById(R.id.countdownView);
+
+                        //COINTDOWN TIMER
+                        if (countdownHasStarted == false) {
+                            countdownHasStarted = true;
+
+                            new CountDownTimer(totalTime, 1000) {
+                                public void onTick(long millisUntilFinished) {
+
+                                    //Convert to minutes and seconds
+                                    cEdit.setText("" + String.format(FORMAT,
+                                            TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                                            TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
+                                                    TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                                            TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
+                                                    TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+                                }
+
+                                //GET LOCATION OF PHONE
+                                Location lastLocation;
+
+                                public void onFinish() {
+                                    cEdit.setText("done!");
+                                    totalTime = 0;
+                                    sendSMSMessage();
+                                    countdownHasStarted = false;
+
+
+                                }
+
+
+                            }
+
+                                    .start();
+                        }
+
                     }
+
+
+                    //SEND SMS MESSAGE
+                    protected void sendSMSMessage() {
+
+                        Log.i("Send SMS", "");
+                        String phoneNo = txtphoneNo.getText().toString();
+                        String  mapLink = "https://www.google.com/maps/?q=" + latitude + "," + longitude;
+                        String message = txtMessage.getText().toString() + "\n Here's my last location: " + mapLink;
+
+
+                        try {
+                            SmsManager smsManager = SmsManager.getDefault();
+                            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+                            Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+                            e.printStackTrace();
+                        }
+                    }
+
+
                 });
+
+        //BLANK SPACE WHEN TEXT IS CLICKED
+        txtphoneNo.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View view) {
+
+
+                    }
+                    void deleteText() {
+                        txtphoneNo.setText("");
+                    }
+        });
     }
 
 
